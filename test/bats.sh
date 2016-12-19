6ead99fc3febd628b3ade701d5468cc4e5eb01f4
Sebastian Sdorra <s.sdorra@gmail.com>
2016-12-19 15:43:19 +0100

    improve test runner

--- test/bats.sh
+++ test/bats.sh
@@ -1,33 +1,43 @@
 #!/bin/sh -e
+
+# variables for bats installation
 BATS_DIR="/tmp/bats"
 BATS_BIN="${BATS_DIR}/bin/bats"
 BATS_URL="https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz"
 
+# tests locations
 TEST_DIR="/test/bats"
 
 # install required dependencies
 
-if ! which bash > /dev/null 2>&1; then
-  apk --update add bash  > /dev/null 2>&1
-fi
+install_dependencies() {
+  if ! which bash; then
+    apk --update add bash
+  fi
 
-if ! which curl > /dev/null 2>&1; then
-  apk --update add curl ca-certificates  > /dev/null 2>&1
-fi
+  if ! which curl; then
+    apk --update add curl ca-certificates
+  fi
 
-if [ ! -f "/usr/bin/tar" ]; then
-  apk --update add tar  > /dev/null 2>&1
-fi
+  if [ ! -f "/usr/bin/tar" ]; then
+    apk --update add tar
+  fi
 
-if [ ! -d "${BATS_DIR}" ]; then
-  mkdir "${BATS_DIR}"  > /dev/null 2>&1
-fi
+  if [ ! -d "${BATS_DIR}" ]; then
+    mkdir "${BATS_DIR}"
+  fi
+}
 
-# install bats
+# install bats with curl
+install_bats() {
+  if [ ! -f "${BATS_BIN}" ]; then
+    cd "${BATS_DIR}" && curl -sL "${BATS_URL}"  | tar --strip-components=1 -xz
+  fi
+}
 
-if [ ! -f "${BATS_BIN}" ]; then
-  cd "${BATS_DIR}" && curl -sL "${BATS_URL}"  | tar --strip-components=1 -xz  > /dev/null 2>&1
-fi
+# installation
+install_dependencies > /dev/null 2>&1
+install_bats > /dev/null 2>&1
 
 # run tests
 
